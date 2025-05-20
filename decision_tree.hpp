3e43051a971fd6732c9eb273c14d60a5f7e59f8b
jingke <820185419@qq.com>
2025-05-20 09:15:34 +0800

    decision tree with any data-type

--- decision_tree.hpp
+++ decision_tree.hpp
@@ -27,39 +27,43 @@ double cal_entropy(const std::vector<double>& samples) {
 	return entropy;
 }
 
-template<int dim_size, typename class_classifier_t>
-double cal_vec_entropy(const std::vector<mat<dim_size, 1, double> >& vdata, class_classifier_t& f_cc)
+template<int dim_size, typename param_t, typename class_classifier_t>
+double cal_vec_entropy(const std::vector<mat<dim_size, 1, param_t> >& vdata, class_classifier_t& f_cc)
 {
-	std::map<int, double> mp;
+	std::map<int, double> mp;                               // 类别到各类的标签的映射
+    // 统计经过类别判别器后的各类的数量
 	for (auto itr = vdata.begin(); itr != vdata.end(); ++itr)
 	{
-		const mat<dim_size, 1, double>& mt = *itr;
+		const mat<dim_size, 1, param_t>& mt = *itr;
 		int i_class = f_cc(mt[dim_size-1]);
 		mp[i_class] = (mp.count(i_class) == 0 ? 1 : mp[i_class] + 1);
 	}
+    // 把数据整理称为用于计算熵值的数组
 	std::vector<double> vec_cnts;
 	for (auto itr = mp.begin(); itr != mp.end(); ++itr)
 	{
 		vec_cnts.push_back(itr->second);
 	}
-	return cal_entropy(vec_cnts);
+	return cal_entropy(vec_cnts);               // 计算所有类别的熵值
 }
 
 /* 计算idx位置分割类的期望熵 */
-template<typename param_classifier_t, typename class_classifier_t, int dim_size>
-double cal_div_entropy(const std::vector<mat<dim_size, 1, double> >& vdata, const int& idx, param_classifier_t& f_pc, class_classifier_t& f_cc)
+template<typename param_classifier_t, typename class_classifier_t, int dim_size, typename param_t>
+double cal_div_entropy(const std::vector<mat<dim_size, 1, param_t> >& vdata, const int& idx, param_classifier_t& f_pc, class_classifier_t& f_cc)
 {
-	std::map<int, std::vector<mat<dim_size, 1, double> > > mp;
+    // 将数据按照idx位置对应的分类器进行分类，划分到不同的类别中
+	std::map<int, std::vector<mat<dim_size, 1, param_t> > > mp;             // 经过分类器判别后的各类数量
 	for (auto itr = vdata.begin(); itr != vdata.end(); ++itr)
 	{
-		const mat<dim_size, 1, double>& mt = *itr;
+		const mat<dim_size, 1, param_t>& mt = *itr;
 		int i_class = f_pc(idx, mt[idx]);
 		if (mp.count(i_class) == 0) 
 		{
-			mp.insert(std::make_pair(i_class, std::vector<mat<dim_size, 1, double> >()));
+			mp.insert(std::make_pair(i_class, std::vector<mat<dim_size, 1, param_t> >()));
 		}
 		mp[i_class].push_back(mt);
 	}
+    // 计算每个类别中实际数据类别的熵值，这样根据某分类器分类后，如果分类效果不佳，那么熵值就会很大；反之，如果分类器效果较好，那么熵值就会很小
 	double d_all_entropy = 0.;
 	for (auto itr = mp.begin(); itr != mp.end(); ++itr) 
 	{
@@ -68,12 +72,14 @@ double cal_div_entropy(const std::vector<mat<dim_size, 1, double> >& vdata, cons
 	return d_all_entropy;
 }
 
-template<typename param_classifier_t, typename class_classifier_t, int dim_size>
-int max_entropy_gain_index(const std::vector<mat<dim_size, 1, double> >& vdata, param_classifier_t& f_pc, class_classifier_t& f_cc)
+// 找到分类最佳的分类器，返回这个分类器的索引
+template<typename param_classifier_t, typename class_classifier_t, int dim_size, typename param_t>
+int max_entropy_gain_index(const std::vector<mat<dim_size, 1, param_t> >& vdata, param_classifier_t& f_pc, class_classifier_t& f_cc)
 {
 	double d_all = cal_vec_entropy(vdata, f_cc);
 	double d_max_gain = -1e10;
 	int i_max_idx = 0;
+    // 循环数组中的所有分类器，找到使的熵值下降最快的那个
 	for (int i = 0; i < dim_size-1; ++i)
 	{
 		double d_cur_gain = d_all - cal_div_entropy(vdata, i, f_pc, f_cc);
@@ -83,45 +89,64 @@ int max_entropy_gain_index(const std::vector<mat<dim_size, 1, double> >& vdata, 
 			i_max_idx = i;
 		}
 	}
-	return i_max_idx;
+	return i_max_idx;           // 返回最佳分类器的索引
 }
 
 /* 根据idx位置的值分割数据集，返回值为idx位置类别到数据集的映射 */
-template<typename param_classifier_t, typename class_classifier_t, int dim_size>
-std::map<int, std::vector<mat<dim_size, 1, double> > > div_data(const std::vector<mat<dim_size, 1, double> >& vdata, const int& idx, param_classifier_t& f_pc, class_classifier_t& f_cc)
+template<typename param_classifier_t, typename class_classifier_t, int dim_size, typename param_t>
+std::map<int, std::vector<mat<dim_size, 1, param_t> > > div_data(const std::vector<mat<dim_size, 1, param_t> >& vdata, const int& idx, param_classifier_t& f_pc, class_classifier_t& f_cc)
 {
-	std::map<int, std::vector<mat<dim_size, 1, double> > > mp;
+	std::map<int, std::vector<mat<dim_size, 1, param_t> > > mp;
 	for (auto itr = vdata.begin(); itr != vdata.end(); ++itr)
 	{
-		const mat<dim_size, 1, double>& mt = *itr;
+		const mat<dim_size, 1, param_t>& mt = *itr;
 		int i_class = f_pc(idx, mt[idx]);
 		if (mp.count(i_class) == 0)
 		{
-			mp.insert(std::make_pair(i_class, std::vector<mat<dim_size, 1, double> >()));
+			mp.insert(std::make_pair(i_class, std::vector<mat<dim_size, 1, param_t> >()));
 		}
 		mp[i_class].push_back(mt);
 	}
 	return mp;
 }
 
-/* 判断类型是不是一类 */
-template<typename class_classifier_t, int dim_size>
-bool same_class(int& i_class, const std::vector<mat<dim_size, 1, double> >& vdata,  class_classifier_t& f_cc)
+/* 判断类型是不是一类，这里可以修改成满足多少比例的为1类就算是该类 */
+template<typename class_classifier_t, int dim_size, typename param_t>
+bool same_class(int& i_class, const std::vector<mat<dim_size, 1, param_t> >& vdata,  class_classifier_t& f_cc, double& rate, const double& class_rate=0.9)
 {
 	i_class = f_cc(vdata[0][dim_size - 1]);
 	if (vdata.size() < 2) 
 	{
+        rate = 1.;
 		return true;
 	}
+    std::map<int, int> mp;		// 统计类别的数量
 	for (int i = 1; i < vdata.size(); ++i) 
 	{
 		int i_cur_class = f_cc(vdata[i][dim_size - 1]);
-		if (i_class != i_cur_class) 
-		{
-			return false;
-		}
+        if (mp.count(i_cur_class) == 0) 
+        {
+            mp.insert(std::make_pair(i_cur_class, 1));
+        }
+        else 
+        {
+            mp[i_cur_class]++;
+        }
 	}
-	return true;
+    // 找到最大分类的类别
+    int i_max_class = -1;
+    int i_max_cnt = 0;
+    for (auto itr = mp.begin(); itr != mp.end(); ++itr)
+    {
+        if (itr->second > i_max_cnt) 
+        {
+            i_max_cnt = itr->second;
+            i_max_class = itr->first;
+        }
+    }
+    // 计算正确率
+    rate = static_cast<double>(i_max_cnt) / vdata.size();
+	return rate >= class_rate;  // 如果正确率大于class_rate，那么就认为是同一类
 }
 
 struct dt_node 
@@ -135,36 +160,38 @@ struct dt_node
 	{}
 };
 
-template<typename param_classifier_t, typename class_classifier_t, int dim_size>
-void _gen_id3_tree(struct dt_node* p_cur_node, const std::vector<mat<dim_size, 1, double> >& vdata, param_classifier_t& f_pc, class_classifier_t& f_cc)
+template<typename param_classifier_t, typename class_classifier_t, int dim_size, typename param_t>
+void _gen_id3_tree(struct dt_node* p_cur_node, const std::vector<mat<dim_size, 1, param_t> >& vdata, param_classifier_t& f_pc, class_classifier_t& f_cc, const double& class_rate=0.9)
 {
 	int i_class = 0;
-	if (same_class(i_class, vdata, f_cc)) 
+    double f_rate = 0.;
+	if (same_class(i_class, vdata, f_cc, f_rate, class_rate)) 
 	{
 		p_cur_node->lbl = i_class;
 		p_cur_node->is_leave = true;
+        p_cur_node->rate = f_rate;
 		return;
 	}
 	p_cur_node->idx = max_entropy_gain_index(vdata, f_pc, f_cc);														// 获取最大分割索引
-	std::map<int, std::vector<mat<dim_size, 1, double> > > mp_div = div_data(vdata, p_cur_node->idx, f_pc, f_cc);		// 分割数据集
+	std::map<int, std::vector<mat<dim_size, 1, param_t> > > mp_div = div_data(vdata, p_cur_node->idx, f_pc, f_cc);		// 分割数据集
 	for (auto itr = mp_div.begin(); itr != mp_div.end(); ++itr)															// 循环判断子集合的决策树
 	{
 		struct dt_node* p_sub_node = new struct dt_node();																// 创建一个新的节点
-		_gen_id3_tree(p_sub_node, itr->second, f_pc, f_cc);																	// 生成子数据集的决策树
+		_gen_id3_tree(p_sub_node, itr->second, f_pc, f_cc, class_rate); 												// 生成子数据集的决策树
 		p_cur_node->mp_sub.insert(std::make_pair(itr->first, p_sub_node));												// 将子决策树加到当前决策树的下面
 	}
 }
 
-template<typename param_classifier_t, typename class_classifier_t, int dim_size>
-dt_node* gen_id3_tree(const std::vector<mat<dim_size, 1, double> >& vdata, param_classifier_t& f_pc, class_classifier_t& f_cc)
+template<typename param_classifier_t, typename class_classifier_t, int dim_size, typename param_t>
+dt_node* gen_id3_tree(const std::vector<mat<dim_size, 1, param_t> >& vdata, param_classifier_t& f_pc, class_classifier_t& f_cc, const double& class_rate=0.9)
 {
 	struct dt_node* p_tree = new struct dt_node();
-	_gen_id3_tree(p_tree, vdata, f_pc, f_cc);
+	_gen_id3_tree(p_tree, vdata, f_pc, f_cc, class_rate);
 	return p_tree;
 }
 
-template<typename param_classifier_t, int dim_size>
-int judge_id3(struct dt_node* p_cur_node, const mat<dim_size, 1, double>& data, param_classifier_t& f_pc, const int& def_value)
+template<typename param_classifier_t, int dim_size, typename param_t>
+int judge_id3(struct dt_node* p_cur_node, const mat<dim_size, 1, param_t>& data, param_classifier_t& f_pc, const int& def_value)
 {
 	if (p_cur_node->is_leave) 
 	{
@@ -178,17 +205,19 @@ int judge_id3(struct dt_node* p_cur_node, const mat<dim_size, 1, double>& data, 
 	return judge_id3(p_cur_node->mp_sub[i_next_idx], data, f_pc, def_value);
 }
 
-template<typename param_classifier_t, typename class_classifier_t, int dim_size>
-std::tuple<double, double> cal_expect_entropy_and_iv(const std::vector<mat<dim_size, 1, double> >& vdata, const int& idx, param_classifier_t& f_pc, class_classifier_t& f_cc)
+/* C4.5 */
+// c4.5算法计算分类的时候会根据数据量对分类器进行加权
+template<typename param_classifier_t, typename class_classifier_t, int dim_size, typename param_t>
+std::tuple<double, double> cal_expect_entropy_and_iv(const std::vector<mat<dim_size, 1, param_t> >& vdata, const int& idx, param_classifier_t& f_pc, class_classifier_t& f_cc)
 {
-	std::map<int, std::vector<mat<dim_size, 1, double> > > mp;				// 按照idx进行分类，分类后的类别到数据的映射就是这个
+	std::map<int, std::vector<mat<dim_size, 1, param_t> > > mp;				// 按照idx进行分类，分类后的类别到数据的映射就是这个
 	for (auto itr = vdata.begin(); itr != vdata.end(); ++itr)
 	{
-		const mat<dim_size, 1, double>& mt = *itr;
+		const mat<dim_size, 1, param_t>& mt = *itr;
 		int i_class = f_pc(idx, mt[idx]);
 		if (mp.count(i_class) == 0)
 		{
-			mp.insert(std::make_pair(i_class, std::vector<mat<dim_size, 1, double> >()));
+			mp.insert(std::make_pair(i_class, std::vector<mat<dim_size, 1, param_t> >()));
 		}
 		mp[i_class].push_back(mt);
 	}
@@ -203,8 +232,9 @@ std::tuple<double, double> cal_expect_entropy_and_iv(const std::vector<mat<dim_s
 	return std::make_tuple(d_all_entropy, iv);
 }
 
-template<typename param_classifier_t, typename class_classifier_t, int dim_size>
-int max_entropy_gain_ratio_index(const std::vector<mat<dim_size, 1, double> >& vdata, param_classifier_t& f_pc, class_classifier_t& f_cc)
+// 使用c4.5的方法计算最大比例的分类器
+template<typename param_classifier_t, typename class_classifier_t, int dim_size, typename param_t>
+int max_entropy_gain_ratio_index(const std::vector<mat<dim_size, 1, param_t> >& vdata, param_classifier_t& f_pc, class_classifier_t& f_cc)
 {
 	double d_all = cal_vec_entropy(vdata, f_cc);
 	double d_max_gain = -1e10;
@@ -223,36 +253,38 @@ int max_entropy_gain_ratio_index(const std::vector<mat<dim_size, 1, double> >& v
 	return i_max_idx;
 }
 
-template<typename param_classifier_t, typename class_classifier_t, int dim_size>
-void _gen_c45_tree(struct dt_node* p_cur_node, const std::vector<mat<dim_size, 1, double> >& vdata, param_classifier_t& f_pc, class_classifier_t& f_cc)
+template<typename param_classifier_t, typename class_classifier_t, int dim_size, typename param_t>
+void _gen_c45_tree(struct dt_node* p_cur_node, const std::vector<mat<dim_size, 1, param_t> >& vdata, param_classifier_t& f_pc, class_classifier_t& f_cc, const double& class_rate=0.9)
 {
 	int i_class = 0;
-	if (same_class(i_class, vdata, f_cc))
-	{
-		p_cur_node->lbl = i_class;
-		p_cur_node->is_leave = true;
-		return;
-	}
+    double f_rate = 0.;
+	if (same_class(i_class, vdata, f_cc, f_rate, class_rate)) 
+    {
+        p_cur_node->lbl = i_class;
+        p_cur_node->is_leave = true;
+        p_cur_node->rate = f_rate;
+        return;
+    }
 	p_cur_node->idx = max_entropy_gain_ratio_index(vdata, f_pc, f_cc);														// 获取最大分割索引
-	std::map<int, std::vector<mat<dim_size, 1, double> > > mp_div = div_data(vdata, p_cur_node->idx, f_pc, f_cc);		// 分割数据集
+	std::map<int, std::vector<mat<dim_size, 1, param_t> > > mp_div = div_data(vdata, p_cur_node->idx, f_pc, f_cc);		// 分割数据集
 	for (auto itr = mp_div.begin(); itr != mp_div.end(); ++itr)															// 循环判断子集合的决策树
 	{
 		struct dt_node* p_sub_node = new struct dt_node();																// 创建一个新的节点
-		_gen_c45_tree(p_sub_node, itr->second, f_pc, f_cc);																	// 生成子数据集的决策树
+		_gen_c45_tree(p_sub_node, itr->second, f_pc, f_cc, class_rate);																	// 生成子数据集的决策树
 		p_cur_node->mp_sub.insert(std::make_pair(itr->first, p_sub_node));												// 将子决策树加到当前决策树的下面
 	}
 }
 
-template<typename param_classifier_t, typename class_classifier_t, int dim_size>
-dt_node* gen_c45_tree(const std::vector<mat<dim_size, 1, double> >& vdata, param_classifier_t& f_pc, class_classifier_t& f_cc)
+template<typename param_classifier_t, typename class_classifier_t, int dim_size, typename param_t>
+dt_node* gen_c45_tree(const std::vector<mat<dim_size, 1, param_t> >& vdata, param_classifier_t& f_pc, class_classifier_t& f_cc, const double& class_rate=0.9)
 {
 	struct dt_node* p_tree = new struct dt_node();
-	_gen_c45_tree(p_tree, vdata, f_pc, f_cc);
+	_gen_c45_tree(p_tree, vdata, f_pc, f_cc, class_rate);
 	return p_tree;
 }
 
-template<typename param_classifier_t, int dim_size>
-int judge_c45(struct dt_node* p_cur_node, const mat<dim_size, 1, double>& data, param_classifier_t& f_pc, const int& def_value)
+template<typename param_classifier_t, int dim_size, typename param_t>
+int judge_c45(struct dt_node* p_cur_node, const mat<dim_size, 1, param_t>& data, param_classifier_t& f_pc, const int& def_value)
 {
 	if (p_cur_node->is_leave)
 	{
